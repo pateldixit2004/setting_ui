398791909fb99a374b975191a853d7dd892ce348
userdixit <120080979+userdixit@users.noreply.github.com>
2023-04-29 09:01:25 +0530

    ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:setting_ui/playstore_ui/playstore_android/provider/playstore_provider.dart';
 import 'package:setting_ui/playstore_ui/playstore_android/view/install_view_ui.dart';
-import 'package:setting_ui/playstore_ui/playstore_android/view/playstore_ui.dart';
-import 'package:setting_ui/playstore_ui/playstore_android/view/top_charts_ui.dart';
-import 'package:setting_ui/setting_ui/android_ui/view/setting_android.dart';
-void main()
-{
+import 'package:setting_ui/setting_ui/ios_ui/view/setting_ui.dart';
+
+void main() {
   runApp(
     // CupertinoApp(
     //   debugShowCheckedModeBanner: false,
@@ -17,19 +17,27 @@ void main()
     // ),,
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (context) => playstoreProvider(),)
+        ChangeNotifierProvider(
+          create: (context) => playstoreProvider(),
+        )
       ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: 'playstore android',
-        routes: {
-          // 'setting ui':(context) => Setting_Android(),
-          // 'playstore android':(context) => Playstore_android(),
-          // 'top screen':(context) => Topscreen(),
-          '/':(context) => Install_Screen(),
-        },
-      ),
-    )
-
+      child:
+           CupertinoApp(
+              debugShowCheckedModeBanner: false,
+              routes: {
+                '/':(p0) => Setting_ios(),
+              },
+            )
+          // : MaterialApp(
+          //     debugShowCheckedModeBanner: false,
+          //     initialRoute: 'playstore android',
+          //     routes: {
+          //       // 'setting ui':(context) => Setting_Android(),
+          //       // 'playstore android':(context) => Playstore_android(),
+          //       // 'top screen':(context) => Topscreen(),
+          //       'instal ': (context) => Install_Screen(),
+          //     },
+          //   ),
+    ),
   );
 }
--- lib/setting_ui/ios_ui/view/setting_ui.dart
+++ lib/setting_ui/ios_ui/view/setting_ui.dart
@@ -10,15 +10,129 @@ class Setting_ios extends StatefulWidget {
 class _Setting_iosState extends State<Setting_ios> {
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(navigationBar: CupertinoNavigationBar(
-      middle: Text("IOS"),
-      leading: Icon(CupertinoIcons.back),
-    ),child: Center(
-      child: Container(
-        height: 100,
-        width: 100,
-        child: Text("Ios"),
-      ),
-    ));
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text(
+            "Setting UI",
+            style: TextStyle(color: CupertinoColors.white),
+          ),
+          backgroundColor: CupertinoColors.destructiveRed,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Common"),
+            Container(
+              color: CupertinoColors.white,
+              child: Column(
+                children: [
+                  CupertinoListTile(
+                    title: Text("Language"),
+                    leading: Icon(CupertinoIcons.globe,color: CupertinoColors.systemGrey),
+                    trailing: Row(
+                      children: [
+                        Text("English",style: TextStyle(color: CupertinoColors.systemGrey),),
+                        Icon(CupertinoIcons.forward,color: CupertinoColors.systemGrey,)
+                      ],
+                    ),
+                  ),
+                  CupertinoListTile(
+                    title: Text("Environment"),
+                    leading: Icon(CupertinoIcons.cloud,color: CupertinoColors.systemGrey),
+                    trailing: Row(
+                      children: [
+                        Text("production",style: TextStyle(color: CupertinoColors.systemGrey),),
+                        Icon(CupertinoIcons.forward,color: CupertinoColors.systemGrey,)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text("Account"),
+            Container(
+              color: CupertinoColors.white,
+              child: Column(
+                children: [
+                  CupertinoListTile(
+                    title: Text("Phone number"),
+                    leading: Icon(CupertinoIcons.phone_fill,color: CupertinoColors.systemGrey),
+                    trailing: Icon(CupertinoIcons.forward,color: CupertinoColors.systemGrey,),
+                  ),
+                  CupertinoListTile(
+                    title: Text("Email"),
+                    leading: Icon(CupertinoIcons.mail,color: CupertinoColors.systemGrey),
+                    trailing: Row(
+                      children: [
+                        Icon(CupertinoIcons.forward,color: CupertinoColors.systemGrey,)
+                      ],
+                    ),
+                  ),
+                  CupertinoListTile(
+                    title: Text("Sign out "),
+                    leading: Icon(CupertinoIcons.square_arrow_right,color: CupertinoColors.systemGrey),
+                    trailing: Row(
+                      children: [
+                        Icon(CupertinoIcons.forward,color: CupertinoColors.systemGrey,)
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Text("Secutiry"),
+            Container(
+              color: CupertinoColors.white,
+              child: Column(
+                children: [
+                  CupertinoListTile(
+                    title: Text("lock app in background"),
+                    leading: Icon(CupertinoIcons.device_phone_portrait,color: CupertinoColors.systemGrey),
+                    trailing: CupertinoSwitch(activeColor: CupertinoColors.destructiveRed,value: true, onChanged: (value) {
+
+                    },)
+                  ),
+                  CupertinoListTile(
+                      title: Text("Use fingerprint"),
+                      leading: Icon(CupertinoIcons.f_cursive_circle_fill,color: CupertinoColors.systemGrey),
+                      trailing: CupertinoSwitch(activeColor: CupertinoColors.white,value: false, onChanged: (value) {
+
+                    },)
+                  ),
+                  CupertinoListTile(
+                    title: Text("Change password"),
+                    leading: Icon(CupertinoIcons.lock_fill,color: CupertinoColors.systemGrey),
+                    trailing: CupertinoSwitch(activeColor: CupertinoColors.destructiveRed,value: true, onChanged: (value) {
+
+                    },)
+                  ),
+
+                ],
+              ),
+            ) ,
+            Text("minc"),
+            Container(
+              color: CupertinoColors.white,
+              child: Column(
+                children: [
+                  CupertinoListTile(
+                    title: Text("Terms of Service"),
+                    leading: Icon(CupertinoIcons.table,color: CupertinoColors.systemGrey),
+                    trailing:                         Icon(CupertinoIcons.forward,color: CupertinoColors.systemGrey,)
+
+                  ),
+                  CupertinoListTile(
+                      title: Text("Open source lincenses"),
+                      leading: Icon(CupertinoIcons.sort_down_circle,color: CupertinoColors.systemGrey),
+                      trailing:                         Icon(CupertinoIcons.forward,color: CupertinoColors.systemGrey,)
+
+                  ),
+
+
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
